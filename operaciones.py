784248f3bad4281a30bc22516ff908eb909752eb
jhoan andres ceballos diaz <jhoanceballos13@gmail.com>
2025-08-12 09:57:21 -0500

    operaciones

---
+++ operaciones.py
@@ -0,0 +1,18 @@
+#suma de vecores
+import numpy as np
+vector1 = np.array([1, 2, 3])
+vector2 = np.array([4, 5, 6])
+suma = vector1 + vector2 #suma de vectores
+print("suma de vectores:", suma) #imprimir suma de vectores
+#multiplicacion de vectores
+producto = vector1 * vector2 #multiplicacion de vectores
+print("producto de vectores:", producto) #imprimir producto de vectores
+#division de vectores
+division = vector1 / vector2 #division de vectores
+print("division de vectores:", division) #imprimir division de vectores
+#resta de vectores 
+resta = vector1 - vector2 #resta de vectores
+print("resta de vectores:", resta) #imprimir resta de vectores
+#potencia de vectores
+potencia = vector1 ** 2 #potencia de vectores
+print("potencia de vectores:", potencia) #imprimir potencia de vectores
